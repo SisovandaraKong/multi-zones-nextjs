a784d9a894326fc38e70ae69a6c8f7c2fd298368
SisovandaraKong <sisovandara@gmail.com>
2026-02-10 18:06:52 +0700

    error env

--- apps/home/next.config.ts
+++ apps/home/next.config.ts
@@ -1,3 +1,33 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     unoptimized: true,  
+//   },
+//   transpilePackages: ['@repo/ui'],
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/mac/:path*',
+//         destination: process.env.NEXT_PUBLIC_URL_MAC + '/mac/:path*',
+//       },
+//             {
+//         source: '/store/:path*',
+//         destination: process.env.NEXT_PUBLIC_URL_STORE + '/store/:path*',
+//       },
+//       {
+//         source: '/profile/:path*',
+//         destination: 'http://localhost:3003/profile/:path*',
+//       },
+//       {
+//         source: '/card/:path*',
+//         destination: 'http://localhost:3004/card/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -9,19 +39,19 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/mac/:path*',
-        destination: process.env.NEXT_PUBLIC_URL_MAC + '/mac/:path*',
-      },
-            {
-        source: '/store/:path*',
-        destination: process.env.NEXT_PUBLIC_URL_STORE + '/store/:path*',
+        destination: `${process.env.NEXT_PUBLIC_URL_MAC}/mac/:path*`,
       },
       {
-        source: '/profile/:path*',
-        destination: 'http://localhost:3003/profile/:path*',
+        source: '/store/:path*',
+        destination: `${process.env.NEXT_PUBLIC_URL_STORE}/store/:path*`,
       },
       {
         source: '/card/:path*',
-        destination: 'http://localhost:3004/card/:path*',
+        destination: `${process.env.NEXT_PUBLIC_URL_CARD}/card/:path*`,
+      },
+      {
+        source: '/profile/:path*',
+        destination: `${process.env.NEXT_PUBLIC_URL_PROFILE}/profile/:path*`,
       },
     ];
   },
